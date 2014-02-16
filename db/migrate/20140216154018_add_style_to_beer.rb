de2bba3d9bb763d21fd4ddeab3943524c4ef976d
Elias Nygren <elias.nygren@outlook.com>
2014-02-16 19:56:56 +0200

    vko5 kaikki

--- db/migrate/20140216154018_add_style_to_beer.rb
+++ db/migrate/20140216154018_add_style_to_beer.rb
@@ -1,7 +1,5 @@
 class AddStyleToBeer < ActiveRecord::Migration
   def change
     add_column :beers, :style_id, :integer
-
-    Beer.all.each {|b| b.style=Style.find_by(name: b.old_style)}
   end
 end
